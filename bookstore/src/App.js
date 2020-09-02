bb45b5b3f63cbb25a25bd946de001f0caacd9b61
aidarous <aidarous12@gmail.com>
2020-09-02 14:11:53 -0400

    api prints in console.log

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [input, useInput] = useState("");
+  const [input , useInput] = useState(null);
   async function grabAllUsers() {
     try {
-      const res = await axios.get('http://localhost:8080/book//sellBook');
+      const res = await axios.get('http://localhost:8080/book/list');
       console.log(res.data);
     } catch(e) {
       console.error(e, e.message);
     }
-    useEffect(() => {
-      grabAllUsers();
-    })
+    
   }
+
+  useEffect(() => {
+    grabAllUsers();
+  })
   return (
     <div className="App">
       
