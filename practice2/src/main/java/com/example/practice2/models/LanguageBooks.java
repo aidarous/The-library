9400629ad20c55478e24f3340e96b0e89d5932f0
aidarous <aidarous12@gmail.com>
2020-09-07 13:09:47 -0400

    added more intellij content

--- practice2/src/main/java/com/example/practice2/models/LanguageBooks.java
+++ practice2/src/main/java/com/example/practice2/models/LanguageBooks.java
@@ -5,4 +5,19 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Languages")
 public class LanguageBooks {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String bookGenre;
+
+    @Column
+    private String title;
+
+    @Column
+    private String price;
+
 }
