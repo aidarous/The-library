ff13a6f8bf2e66b73d6c1a99323432f8bacaaab5
aidarous <aidarous12@gmail.com>
2020-09-07 21:47:49 -0400

    added more intellij content

--- practice2/src/main/java/com/example/practice2/controllers/LanguageBooksController.java
+++ practice2/src/main/java/com/example/practice2/controllers/LanguageBooksController.java
@@ -2,5 +2,9 @@ package com.example.practice2.controllers;
 
 import com.example.practice2.models.LanguageBooks;
 import com.example.practice2.services.LangaugeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 public class LanguageBooksController {
 }
