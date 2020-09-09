b9ed835de6a2591a54954a22a213d81897ce257e
aidarous <aidarous12@gmail.com>
2020-09-09 04:54:21 -0400

    added more intellij content

---
+++ practice2/src/main/java/com/example/practice2/repositories/LanguageRepositories.java
@@ -0,0 +1,4 @@
+package com.example.practice2.repositories;
+
+public interface LanguageRepositories {
+}
--- practice2/src/main/java/com/example/practice2/services/LanguageBooksImpl.java
+++ practice2/src/main/java/com/example/practice2/services/LanguageBooksImpl.java
@@ -1,4 +1,29 @@
 package com.example.practice2.services;
 
-public class LanguageBooksImpl {
+import com.example.practice2.models.LanguageBooks;
+import com.example.practice2.repositories.BookRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LanguageBooksImpl implements LanguageService {
+
+    @Autowired
+    LanguageRepositories languageRepositories;
+
+    @Override
+    public Iterable<LanguageBooks> listLanguageBooks() {
+        return languageRepositories.findAll();
+    }
+
+    @Override
+    public LanguageBooks createLanguageBooks(LanguageBooks languageBooks){
+        return languageRepositories.save(languageBooks);
+    }
+    @Override
+    public HttpStatus deleteLanguageBooksById(Long id) {
+        languageRepositories.deleteById(id);
+        return HttpStatus.OK;
+    }
 }
